de1012805743c3554b0a52df35f0bed85abefebe
Ricky Hu <ricky9667@gmail.com>
2023-03-25 16:16:02 +0800

    feat: add home screen image

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,8 +3,29 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Widget get _parrotGif => Image.asset(
+        'assets/parrot.gif',
+        fit: BoxFit.contain,
+      );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ConstrainedBox(
+                constraints: BoxConstraints.loose(const Size.square(160)),
+                child: _parrotGif,
+              ),
+              const Text('Parrot Number'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
