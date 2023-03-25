9163bc7cc6bdbaa6af9b3ed673fec0bbe36ef2e2
Ricky Hu <ricky9667@gmail.com>
2023-03-25 15:25:22 +0800

    feat: create home, guess, and result page

---
+++ lib/pages/guess_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GuessPage extends StatelessWidget {
+  const GuessPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
