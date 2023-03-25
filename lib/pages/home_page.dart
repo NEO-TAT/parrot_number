40ff4b849c8fe331c44731f684ebec5f487e6cfa
Ricky Hu <ricky9667@gmail.com>
2023-03-25 16:32:32 +0800

    feat: add widgets and background to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,20 +8,70 @@ class HomePage extends StatelessWidget {
         fit: BoxFit.contain,
       );
 
+  Widget get _titleText => const Text(
+        'Parrot Number',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+
+  Widget get _startGameButton => ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(240, 48),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+          ),
+          textStyle: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          backgroundColor: Colors.black,
+        ),
+        child: const Text('Start Game'),
+      );
+
+  LinearGradient get _rainbowBackground => const LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Color(0xFFE91E63),
+          Color(0xFF9C27B0),
+          Color(0xFF673AB7),
+          Color(0xFF3F51B5),
+          Color(0xFF2196F3),
+          Color(0xFF03A9F4),
+          Color(0xFF00BCD4),
+          Color(0xFF009688),
+          Color(0xFF4CAF50),
+          Color(0xFF8BC34A),
+          Color(0xFFCDDC39),
+          Color(0xFFFFEB3B),
+          Color(0xFFFFC107),
+          Color(0xFFFF9800),
+          Color(0xFFFF5722),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SizedBox(
-          width: double.infinity,
+      body: Container(
+        decoration: BoxDecoration(gradient: _rainbowBackground),
+        width: double.infinity,
+        child: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               ConstrainedBox(
                 constraints: BoxConstraints.loose(const Size.square(160)),
                 child: _parrotGif,
               ),
-              const Text('Parrot Number'),
+              _titleText,
+              _startGameButton,
             ],
           ),
         ),
