e500d85640024a10ec1fb599a94805d1f6020095
Ricky Hu <ricky9667@gmail.com>
2023-04-16 23:59:24 +0800

    feat: add Hero animation to ParrotGif

--- lib/widgets/parrot_gif.dart
+++ lib/widgets/parrot_gif.dart
@@ -4,8 +4,8 @@ class ParrotGif extends StatelessWidget {
   const ParrotGif({super.key});
 
   @override
-  Widget build(BuildContext context) => Image.asset(
-        'assets/parrot.gif',
-        fit: BoxFit.contain,
+  Widget build(BuildContext context) => Hero(
+        tag: 'parrot',
+        child: Image.asset('assets/parrot.gif', fit: BoxFit.contain),
       );
 }
