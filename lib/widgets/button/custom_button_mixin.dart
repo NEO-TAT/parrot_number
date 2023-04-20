b461a7f59bb77e801ee62f82f33f25d62cb17f0d
Ricky Hu <ricky9667@gmail.com>
2023-04-21 00:56:41 +0800

    style: format files

--- lib/widgets/button/custom_button_mixin.dart
+++ lib/widgets/button/custom_button_mixin.dart
@@ -2,14 +2,14 @@ import 'package:flutter/widgets.dart';
 
 mixin CustomButtonMixin on Widget {
   TextStyle get buttonTextStyle => const TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.bold,
-    fontFamily: 'TATFont',
-  );
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'TATFont',
+      );
 
   Size get buttonSize => const Size(240, 48);
 
   OutlinedBorder get buttonShape => const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(32)),
-  );
+        borderRadius: BorderRadius.all(Radius.circular(32)),
+      );
 }
